45cc3bac2a6e32c81086e50302ebf2040673af76
Dai Williams <Daio-io@users.noreply.github.com>
2024-08-17 15:09:41 +0100

    Add support to publish to GitHub packages (#5)
    * Add support to publish to GitHub packages

--- gradle/build-logic/convention/src/main/kotlin/io/daio/gradle/PublishingConventionPlugin.kt
+++ gradle/build-logic/convention/src/main/kotlin/io/daio/gradle/PublishingConventionPlugin.kt
@@ -6,5 +6,9 @@ import org.gradle.api.Project
 class PublishingConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
         pluginManager.apply("com.vanniktech.maven.publish")
+
+        if (project.hasProperty("useGitHubPublishing")) {
+            configureGitHubPublishing()
+        }
     }
 }
