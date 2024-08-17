5006f8bf40665a162be426636a275e2f9b6f751e
Dai Williams <Daio-io@users.noreply.github.com>
2024-08-17 16:43:34 +0100

    Set version name from properties and disable signing (#6)

--- gradle/build-logic/convention/src/main/kotlin/io/daio/gradle/GitHubPackagesPublishing.kt
+++ gradle/build-logic/convention/src/main/kotlin/io/daio/gradle/GitHubPackagesPublishing.kt
@@ -6,23 +6,19 @@ import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.provideDelegate
 
 internal fun Project.configureGitHubPublishing() {
-    val gitHubPackagesUsername: String? by project
-    val gitHubPackagesPassword: String? by project
+    val githubUser: String? by project
+    val githubToken: String? by project
     val gitHubPackageUri: String? by project
     val gitCommit: String? by project
 
-    val user =
-        gitHubPackagesUsername ?: project.findProperty("gitHubPackagesUsername").toString()
-    val pass = gitHubPackagesPassword ?: project.findProperty("gitHubPackagesPassword").toString()
-    val commit = gitCommit ?: project.findProperty("gitCommit").toString()
-    val versionName = project.findProperty("VERSION_NAME").toString()
+    val user = githubUser ?: project.findProperty("githubUser").toString()
+    val pass = githubToken ?: project.findProperty("githubToken").toString()
 
     publishing {
         repositories {
             maven {
                 name = "GitHubPackages"
                 url = uri("https://maven.pkg.github.com/daio-io/wild")
-                version = "${versionName}-$commit"
                 credentials {
                     username = user
                     password = pass
