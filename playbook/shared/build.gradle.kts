4e02ec73978b69a908d78ad5b3af2dc0ef3b1a13
daio-io <dle.williams1@gmail.com>
2024-08-31 17:07:54 -0600

    Make non Tv packages multiplatform

--- playbook/shared/build.gradle.kts
+++ playbook/shared/build.gradle.kts
@@ -14,6 +14,7 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 api(compose.foundation)
+                api(projects.foundations)
             }
         }
 
