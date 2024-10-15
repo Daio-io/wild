823fbd6a0adfa56c985f48bdd9e7869621212fc6
daio-io <dle.williams1@gmail.com>
2024-10-15 09:06:15 +0100

    Make some minor tweaks to clickables and start of samples

--- playbook/shared/src/jvmMain/kotlin/io/daio/common/DesktopApp.kt
+++ playbook/shared/src/jvmMain/kotlin/io/daio/common/DesktopApp.kt
@@ -6,5 +6,5 @@ import androidx.compose.runtime.Composable
 @Preview
 @Composable
 private fun AppPreview() {
-    App()
+    CustomDesignSystemApp()
 }
---
+++ style/src/commonMain/kotlin/io/daio/wild/style/Clickable.kt
@@ -0,0 +1,93 @@
+package io.daio.wild.style
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.semantics.Role
+import io.daio.wild.foundation.clickable
+import io.daio.wild.foundation.selectable
+import io.daio.wild.modifier.thenIfNotNull
+
+/**
+ * Interop Modifier.clickable to apply the correct clickable modifier based on the requirement for
+ * hardware input. For example if a Tv device is detected it adds support for hardware clicks from
+ * remote controls. This has the added support for [Style], applying [interactionStyle] to update
+ * the component based on the current [InteractionSource] state.
+ *
+ * @param enabled Whether the click action handling is enabled.
+ * @param interactionSource The interaction source to emit interaction events to.
+ * @param style Optional [Style] to apply with the clickable.
+ * @param role The Role of the associated user interface element, typically used by Accessiblity
+ * services.
+ * @param onLongClick Optional callback to handle long click events.
+ * @param onClick Callback when the element is clicked.
+ */
+fun Modifier.clickable(
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource? = null,
+    style: Style? = null,
+    role: Role? = null,
+    onLongClick: (() -> Unit)? = null,
+    onClick: (() -> Unit),
+) = composed {
+    @Suppress("NAME_SHADOWING")
+    val interactionSource = interactionSource ?: remember { MutableInteractionSource() }
+
+    Modifier.clickable(
+        enabled = enabled,
+        interactionSource = interactionSource,
+        role = role,
+        onClick = onClick,
+        onLongClick = onLongClick,
+    ).thenIfNotNull(
+        style,
+        ifNotNullModifier = {
+            Modifier.interactionStyle(it, interactionSource, enabled)
+        },
+    )
+}
+
+/**
+ * Interop Modifier.selectable to apply the correct selectable modifier based on the requirement for
+ * hardware input. For example if a Tv device is detected it adds support for hardware clicks from
+ * remote controls. This has the added support for [Style], applying [interactionStyle] to update
+ *  * the component based on the current [InteractionSource] state.
+ *
+ * @param selected Whether the element is currently selected.
+ * @param enabled Whether the click action handling is enabled.
+ * @param interactionSource The interaction source to emit interaction events to.
+ * @param style Optional [Style] to apply with the selectable.
+ * @param role The Role of the associated user interface element, typically used by Accessiblity
+ * services.
+ * @param onClick Callback when the element is clicked.
+ */
+fun Modifier.selectable(
+    selected: Boolean,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource? = null,
+    style: Style? = null,
+    role: Role? = null,
+    onClick: (() -> Unit),
+) = composed {
+    @Suppress("NAME_SHADOWING")
+    val interactionSource = interactionSource ?: remember { MutableInteractionSource() }
+
+    Modifier.selectable(
+        selected = selected,
+        enabled = enabled,
+        interactionSource = interactionSource,
+        role = role,
+        onClick = onClick,
+    ).thenIfNotNull(
+        value = style,
+        ifNotNullModifier = {
+            Modifier.interactionStyle(
+                style = it,
+                interactionSource = interactionSource,
+                enabled = enabled,
+                selected = selected,
+            )
+        },
+    )
+}
