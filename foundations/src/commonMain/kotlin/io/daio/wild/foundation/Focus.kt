2a1b77c2706a1a8aa6d88e70ec0e088bdefec327
daio-io <dle.williams1@gmail.com>
2024-10-19 16:51:52 +0100

    Add an experimental initial Focus requester modifier.

--- foundations/src/commonMain/kotlin/io/daio/wild/foundation/Focus.kt
+++ foundations/src/commonMain/kotlin/io/daio/wild/foundation/Focus.kt
@@ -6,16 +6,111 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.FocusRequesterModifierNode
 import androidx.compose.ui.focus.focusRestorer
+import androidx.compose.ui.focus.requestFocus
 import androidx.compose.ui.focus.restoreFocusedChild
 import androidx.compose.ui.focus.saveFocusedChild
 import androidx.compose.ui.input.key.KeyEvent
 import androidx.compose.ui.input.key.KeyInputModifierNode
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.layout.LayoutCoordinates
-import androidx.compose.ui.node.CompositionLocalConsumerModifierNode
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.node.GlobalPositionAwareModifierNode
 import androidx.compose.ui.node.ModifierNodeElement
 import androidx.compose.ui.platform.InspectorInfo
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+/**
+ * Utility [Modifier] to request focus on initial layout. This Modifier calls to [requestFocus] when
+ * [onGloballyPositioned] is set, caching the fact it has been positioned to make sure any request
+ * to focus is only called once after positioning.
+ *
+ * This can be used on a [focusGroup] or on any focusable composable.
+ *
+ * @param enabled Whether or not the request to focus should be enabled. Setting this to false and
+ * back to true will allow focus to be re-requested.
+ * @param delay Optional delay before [requestFocus] is called. Long delay is safe here as the node
+ * will cancel the request to requestFocus if detracted while delayed.
+ *
+ *
+ * @since 0.3.0
+ *
+ * Example:
+ * LazyColumn {
+ *     items(20) {
+ *         LazyRow(
+ *             modifier = Modifier
+ *                 .fillMaxSize()
+ *                 .focusGroup()
+ *                 .requestInitialFocus()
+ *         ) {
+ *             items(100) {
+ *                 Button(onClick = onClick) {
+ *                     BasicText("Clickable")
+ *                 }
+ *             }
+ *         }
+ *     }
+ * }
+ * ```
+ *
+ */
+fun Modifier.requestInitialFocus(
+    enabled: Boolean = true,
+    delay: Long = 0L,
+): Modifier {
+    return if (!enabled) Modifier else RequestFocusElement(delay)
+}
+
+private class RequestFocusNode(var focusDelay: Long) :
+    FocusRequesterModifierNode,
+    GlobalPositionAwareModifierNode,
+    Modifier.Node() {
+    private var positioned: Boolean = false
+
+    override fun onGloballyPositioned(coordinates: LayoutCoordinates) {
+        if (!positioned && isAttached) {
+            positioned = true
+            requestInitialFocus(focusDelay)
+        }
+    }
+
+    private fun requestInitialFocus(focusDelay: Long) {
+        if (focusDelay <= 0) {
+            requestFocus()
+            return
+        }
+
+        coroutineScope.launch {
+            delay(focusDelay)
+            requestFocus()
+        }
+    }
+}
+
+private class RequestFocusElement(val delay: Long) : ModifierNodeElement<RequestFocusNode>() {
+    override fun create() = RequestFocusNode(delay)
+
+    override fun update(node: RequestFocusNode) {
+        node.focusDelay = delay
+    }
+
+    override fun InspectorInfo.inspectableProperties() {
+        name = "requestFocus"
+        properties["delay"] = delay
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+
+        other as RequestFocusElement
+
+        return delay == other.delay
+    }
+
+    override fun hashCode(): Int = delay.hashCode()
+}
 
 /**
  * Utility [Modifier] to restore focus of a child within a focus group. A common use case
@@ -50,8 +145,7 @@ fun Modifier.restoreChildFocus(onRestoreFailed: (() -> FocusRequester)? = null):
     this.focusGroup()
         .focusRestorer(onRestoreFailed) then RestoreChildFocusElement()
 
-internal class RestoreChildNode : CompositionLocalConsumerModifierNode,
-    FocusRequesterModifierNode,
+private class RestoreChildNode : FocusRequesterModifierNode,
     KeyInputModifierNode,
     GlobalPositionAwareModifierNode,
     Modifier.Node() {
@@ -64,12 +158,10 @@ internal class RestoreChildNode : CompositionLocalConsumerModifierNode,
 
     @OptIn(ExperimentalComposeUiApi::class)
     override fun onGloballyPositioned(coordinates: LayoutCoordinates) {
-        if (positioned) {
-            return
+        if (!positioned && isAttached) {
+            restoreFocusedChild()
+            positioned = true
         }
-
-        restoreFocusedChild()
-        positioned = true
     }
 
     override fun onKeyEvent(event: KeyEvent): Boolean = false
