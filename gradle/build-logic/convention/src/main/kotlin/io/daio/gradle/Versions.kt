a22d8c5a3b5b85493b008187119020fd3fe231d9
Dai Williams <Daio-io@users.noreply.github.com>
2024-10-31 06:57:01 +0000

    Bump SDK versions target and compile (#72)

--- gradle/build-logic/convention/src/main/kotlin/io/daio/gradle/Versions.kt
+++ gradle/build-logic/convention/src/main/kotlin/io/daio/gradle/Versions.kt
@@ -6,9 +6,9 @@ import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.getByType
 
 object Versions {
-    const val COMPILE_SDK = 34
+    const val COMPILE_SDK = 35
     const val MIN_SDK = 23
-    const val TARGET_SDK = 34
+    const val TARGET_SDK = 35
 }
 
 val Project.libs: VersionCatalog
