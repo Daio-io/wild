03359e8d9a6df1842e734141d724a90eace2b0b5
Dai Williams <Daio-io@users.noreply.github.com>
2024-11-16 20:20:03 +0000

    Improve interaction Modifiers (#86)

--- style/src/commonMain/kotlin/io/daio/wild/style/Clickable.kt
+++ style/src/commonMain/kotlin/io/daio/wild/style/Clickable.kt
@@ -10,6 +10,55 @@ import io.daio.wild.foundation.selectable
 import io.daio.wild.modifier.thenIfNotNull
 
 /**
+ * Interop Modifier to support either [Modifier.selectable] or [Modifier.clickable], applying
+ * the correct modifier based on the requirement for hardware input. For example if a Tv device
+ * is detected it adds support for hardware clicks from remote controls. This has the added support
+ * for [Style], applying [interactionStyle] to update the component based on the current
+ * [InteractionSource] state.
+ *
+ * @param enabled Whether the click action handling is enabled.
+ * @param selected Optional property to set the selected state. Setting this to a value will enable
+ * selectable support.
+ * @param interactionSource The interaction source to emit interaction events to.
+ * @param role The Role of the associated user interface element, typically used by Accessiblity
+ * services.
+ * @param onLongClick Optional callback to handle long click events.
+ * @param onClick Callback when the element is clicked.
+ *
+ * @since 0.3.1
+ */
+fun Modifier.interactable(
+    enabled: Boolean = true,
+    selected: Boolean? = null,
+    style: Style? = null,
+    interactionSource: MutableInteractionSource? = null,
+    role: Role? = null,
+    onLongClick: (() -> Unit)? = null,
+    onClick: (() -> Unit),
+): Modifier =
+    this then
+        if (selected != null) {
+            Modifier.selectable(
+                selected = selected,
+                style = style,
+                enabled = enabled,
+                interactionSource = interactionSource,
+                role = role,
+                onLongClick = onLongClick,
+                onClick = onClick,
+            )
+        } else {
+            Modifier.clickable(
+                enabled = enabled,
+                style = style,
+                interactionSource = interactionSource,
+                role = role,
+                onClick = onClick,
+                onLongClick = onLongClick,
+            )
+        }
+
+/**
  * Interop Modifier.clickable to apply the correct clickable modifier based on the requirement for
  * hardware input. For example if a Tv device is detected it adds support for hardware clicks from
  * remote controls. This has the added support for [Style], applying [interactionStyle] to update
@@ -72,6 +121,7 @@ fun Modifier.selectable(
     interactionSource: MutableInteractionSource? = null,
     style: Style? = null,
     role: Role? = null,
+    onLongClick: (() -> Unit)? = null,
     onClick: (() -> Unit),
 ) = composed {
     @Suppress("NAME_SHADOWING")
@@ -81,6 +131,7 @@ fun Modifier.selectable(
         selected = selected,
         enabled = enabled,
         interactionSource = interactionSource,
+        onLongClick = onLongClick,
         role = role,
         onClick = onClick,
     ).thenIfNotNull(
