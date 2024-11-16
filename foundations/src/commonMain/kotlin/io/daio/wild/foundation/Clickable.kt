4e2bef3eb02b5140fcaabeeb9157ffd928023563
Dai Williams <Daio-io@users.noreply.github.com>
2024-11-16 20:20:03 +0000

    Improve interaction Modifiers (#86)

--- foundations/src/commonMain/kotlin/io/daio/wild/foundation/Clickable.kt
+++ foundations/src/commonMain/kotlin/io/daio/wild/foundation/Clickable.kt
@@ -31,6 +31,54 @@ import androidx.compose.ui.semantics.semantics
 import kotlinx.coroutines.launch
 
 /**
+ * Interop Modifier to support either [Modifier.selectable] or [Modifier.clickable], applying
+ * the correct modifier based on the requirement for hardware input. For example if a Tv device
+ * is detected it adds support for hardware clicks from remote controls.
+ *
+ * @param enabled Whether the click action handling is enabled.
+ * @param selected Optional property to set the selected state. Setting this to a value will enable
+ * selectable support.
+ * @param interactionSource The interaction source to emit interaction events to.
+ * @param indication Optional indication to apply with the clickable.
+ * @param role The Role of the associated user interface element, typically used by Accessiblity
+ * services.
+ * @param onLongClick Optional callback to handle long click events.
+ * @param onClick Callback when the element is clicked.
+ *
+ * @since 0.3.1
+ */
+fun Modifier.interactable(
+    enabled: Boolean = true,
+    selected: Boolean? = null,
+    interactionSource: MutableInteractionSource? = null,
+    indication: Indication? = null,
+    role: Role? = null,
+    onLongClick: (() -> Unit)? = null,
+    onClick: (() -> Unit),
+): Modifier =
+    this then
+        if (selected != null) {
+            Modifier.selectable(
+                selected = selected,
+                enabled = enabled,
+                interactionSource = interactionSource,
+                indication = indication,
+                role = role,
+                onLongClick = onLongClick,
+                onClick = onClick,
+            )
+        } else {
+            Modifier.clickable(
+                enabled = enabled,
+                interactionSource = interactionSource,
+                indication = indication,
+                role = role,
+                onClick = onClick,
+                onLongClick = onLongClick,
+            )
+        }
+
+/**
  * Interop Modifier.clickable to apply the correct clickable modifier based on the requirement for
  * hardware input. For example if a Tv device is detected it adds support for hardware clicks from
  * remote controls.
@@ -53,30 +101,31 @@ fun Modifier.clickable(
     role: Role? = null,
     onLongClick: (() -> Unit)? = null,
     onClick: (() -> Unit),
-) = composed {
-    @Suppress("NAME_SHADOWING")
-    val interactionSource = interactionSource ?: remember { MutableInteractionSource() }
+): Modifier =
+    composed {
+        @Suppress("NAME_SHADOWING")
+        val interactionSource = interactionSource ?: remember { MutableInteractionSource() }
 
-    if (LocalPlatformInteractions.current.requiresHardwareInput) {
-        Modifier.hardwareClickable(
-            enabled = enabled,
-            interactionSource = interactionSource,
-            indication = indication,
-            role = role,
-            onLongClick = onLongClick,
-            onClick = onClick,
-        )
-    } else {
-        Modifier.combinedClickable(
-            interactionSource = interactionSource,
-            indication = indication,
-            enabled = enabled,
-            role = role,
-            onLongClick = onLongClick,
-            onClick = onClick,
-        )
+        if (LocalPlatformInteractions.current.requiresHardwareInput) {
+            Modifier.hardwareClickable(
+                enabled = enabled,
+                interactionSource = interactionSource,
+                indication = indication,
+                role = role,
+                onLongClick = onLongClick,
+                onClick = onClick,
+            )
+        } else {
+            Modifier.combinedClickable(
+                interactionSource = interactionSource,
+                indication = indication,
+                enabled = enabled,
+                role = role,
+                onLongClick = onLongClick,
+                onClick = onClick,
+            )
+        }
     }
-}
 
 /**
  * Modifier to set up handling of click events using hardware input such as a Tv remote control.
@@ -97,38 +146,20 @@ fun Modifier.hardwareClickable(
     onLongClick: (() -> Unit)? = null,
     indication: Indication? = null,
     onClick: (() -> Unit)?,
-) = handleHardwareInputEnter(
-    enabled = enabled,
-    interactionSource = interactionSource,
-    onClick = onClick,
-    onLongClick = onLongClick,
-)
-    // We are not using "clickable" modifier here because if we set "enabled" to false
-    // then the Surface won't be focusable as well. But, in TV use case, a disabled surface
-    // should be focusable
-    .focusable(interactionSource = interactionSource)
-    .semantics(mergeDescendants = true) {
-        if (role != null) {
-            this.role = role
-        }
-        onClick {
-            onClick?.let { nnOnClick ->
-                nnOnClick()
-                return@onClick true
-            }
-            false
-        }
-        onLongClick {
-            onLongClick?.let { nnOnLongClick ->
-                nnOnLongClick()
-                return@onLongClick true
-            }
-            false
-        }
-        if (!enabled) {
-            disabled()
-        }
-    }.indication(interactionSource, indication)
+): Modifier =
+    handleHardwareInputEnter(
+        enabled = enabled,
+        interactionSource = interactionSource,
+        onClick = onClick,
+        onLongClick = onLongClick,
+    ).hardwareInteractable(
+        enabled = enabled,
+        role = role,
+        interactionSource = interactionSource,
+        indication = indication,
+        onClick = onClick,
+        onLongClick = onLongClick,
+    )
 
 /**
  * Interop Modifier.selectable to apply the correct selectable modifier based on the requirement for
@@ -151,30 +182,34 @@ fun Modifier.selectable(
     interactionSource: MutableInteractionSource? = null,
     indication: Indication? = null,
     role: Role? = null,
+    onLongClick: (() -> Unit)? = null,
     onClick: (() -> Unit),
-) = composed {
-    @Suppress("NAME_SHADOWING")
-    val interactionSource = interactionSource ?: remember { MutableInteractionSource() }
+): Modifier =
+    composed {
+        @Suppress("NAME_SHADOWING")
+        val interactionSource = interactionSource ?: remember { MutableInteractionSource() }
 
-    if (LocalPlatformInteractions.current.requiresHardwareInput) {
-        Modifier.hardwareSelectable(
-            selected = selected,
-            enabled = enabled,
-            interactionSource = interactionSource,
-            role = role,
-            onClick = onClick,
-        )
-    } else {
-        Modifier.selectable(
-            selected = selected,
-            interactionSource = interactionSource,
-            indication = indication,
-            enabled = enabled,
-            role = role,
-            onClick = onClick,
-        )
+        if (LocalPlatformInteractions.current.requiresHardwareInput) {
+            Modifier.hardwareSelectable(
+                selected = selected,
+                enabled = enabled,
+                interactionSource = interactionSource,
+                role = role,
+                onClick = onClick,
+                onLongClick = onLongClick,
+            )
+        } else {
+            // TODO need to support long click.
+            Modifier.selectable(
+                selected = selected,
+                interactionSource = interactionSource,
+                indication = indication,
+                enabled = enabled,
+                role = role,
+                onClick = onClick,
+            )
+        }
     }
-}
 
 /**
  * Modifier to set up handling of selecting events using hardware input such as a Tv remote control.
@@ -190,43 +225,70 @@ fun Modifier.selectable(
  * @since 0.2.0
  */
 fun Modifier.hardwareSelectable(
-    enabled: Boolean,
-    selected: Boolean = true,
+    selected: Boolean,
+    enabled: Boolean = true,
     interactionSource: MutableInteractionSource,
     role: Role? = null,
     onLongClick: (() -> Unit)? = null,
-    onClick: (() -> Unit),
-) = handleHardwareInputEnter(
-    enabled = enabled,
-    interactionSource = interactionSource,
-    selected = selected,
-    onClick = onClick,
-    onLongClick = onLongClick,
-)
-    // We are not using "selectable" modifier here because if we set "enabled" to false
-    // then the Surface won't be focusable as well. But, in TV use case, a disabled surface
-    // should be focusable
-    .focusable(interactionSource = interactionSource)
-    .semantics(mergeDescendants = true) {
-        if (role != null) {
-            this.role = role
-        }
-        this.selected = selected
-        onClick {
-            onClick()
-            true
-        }
-        onLongClick {
-            onLongClick?.let { nnOnLongClick ->
-                nnOnLongClick()
-                return@onLongClick true
+    indication: Indication? = null,
+    onClick: (() -> Unit)?,
+): Modifier =
+    handleHardwareInputEnter(
+        enabled = enabled,
+        interactionSource = interactionSource,
+        selected = selected,
+        onClick = onClick,
+        onLongClick = onLongClick,
+    ).hardwareInteractable(
+        enabled = enabled,
+        role = role,
+        interactionSource = interactionSource,
+        indication = indication,
+        selected = selected,
+        onClick = onClick,
+        onLongClick = onLongClick,
+    )
+
+private fun Modifier.hardwareInteractable(
+    enabled: Boolean,
+    role: Role?,
+    interactionSource: MutableInteractionSource,
+    indication: Indication? = null,
+    selected: Boolean? = null,
+    onClick: (() -> Unit)? = null,
+    onLongClick: (() -> Unit)? = null,
+): Modifier =
+
+// We are not using "clickable" modifier here because if we set "enabled" to false
+// then the Surface won't be focusable as well. But, in TV use case, a disabled surface
+// should be focusable
+    focusable(interactionSource = interactionSource)
+        .semantics(mergeDescendants = true) {
+            if (selected != null) {
+                this.selected = selected
             }
-            false
-        }
-        if (!enabled) {
-            disabled()
-        }
-    }
+
+            if (role != null) {
+                this.role = role
+            }
+            onClick {
+                onClick?.let { nnOnClick ->
+                    nnOnClick()
+                    return@onClick true
+                }
+                false
+            }
+            onLongClick {
+                onLongClick?.let { nnOnLongClick ->
+                    nnOnLongClick()
+                    return@onLongClick true
+                }
+                false
+            }
+            if (!enabled) {
+                disabled()
+            }
+        }.indication(interactionSource, indication)
 
 /**
  * Modifier to set up handling of hardware input enter keys, such as Tv remote Dpad enter and
