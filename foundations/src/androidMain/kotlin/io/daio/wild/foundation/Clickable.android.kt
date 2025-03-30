99bd9a681471863ad8e1e5f49233424485190fdd
Dai Williams <Daio-io@users.noreply.github.com>
2025-03-30 17:13:14 +0100

    Expose multiplatform repeat key (#141)

--- foundations/src/androidMain/kotlin/io/daio/wild/foundation/Clickable.android.kt
+++ foundations/src/androidMain/kotlin/io/daio/wild/foundation/Clickable.android.kt
@@ -4,5 +4,5 @@ package io.daio.wild.foundation
 
 import androidx.compose.ui.input.key.KeyEvent
 
-internal actual val KeyEvent.repeatCount: Int
+actual val KeyEvent.repeatCount: Int
     get() = nativeKeyEvent.repeatCount
