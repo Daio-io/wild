c80254b9271d282cf98050115e1a6caf1e66f642
Dai Williams <Daio-io@users.noreply.github.com>
2026-04-12 09:08:12 +0100

    feat(site): add storybook web app to GitHub Pages deployment (#271)
    Add the interactive storybook (Compose WASM) to the docs deploy
    pipeline and expose it as "Usage Docs" in the MkDocs sidebar.

--- playbook/web/webpack.config.d/config.js
+++ playbook/web/webpack.config.d/config.js
@@ -1,5 +1,8 @@
 const TerserPlugin = require("terser-webpack-plugin");
 
+config.output = config.output || {};
+config.output.publicPath = "auto";
+
 config.optimization = config.optimization || {};
 config.optimization.minimize = true;
 config.optimization.minimizer = [
