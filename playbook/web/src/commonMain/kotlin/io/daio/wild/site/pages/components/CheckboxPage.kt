c24ec323f758c4c29059eb463141bcdbc2a03500
daio-io <dle.williams1@gmail.com>
2026-09-22 15:07:32 +0100

    loopy: apply changes for THE-479

--- playbook/web/src/commonMain/kotlin/io/daio/wild/site/pages/components/CheckboxPage.kt
+++ playbook/web/src/commonMain/kotlin/io/daio/wild/site/pages/components/CheckboxPage.kt
@@ -20,9 +20,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.unit.dp
+import io.daio.wild.components.text.Text
 import io.daio.wild.components.toggleable.Checkbox
 import io.daio.wild.components.toggleable.TriStateCheckbox
-import io.daio.wild.components.text.Text
 import io.daio.wild.site.components.ComponentPage
 import io.daio.wild.site.components.ComponentPageData
 import io.daio.wild.site.components.Demo
